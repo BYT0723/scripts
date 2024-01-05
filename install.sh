5aa528baf56474152df14bbab6fe5134f9998e95
BYT0723 <twang9739@gmail.com>
2024-01-06 00:09:06 +0800

    chore: install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pacman -S rofi picom lxsession xautolock betterlockscreen slock networkmanager network-manager-applet udiskie fcitx5-im light libnotify acpi alsa-utils mpc mpd mpv bc bluez bluez-utils firefox alacritty feh xwinwrap-git archlinux-wallpaper maim viewnior surf
