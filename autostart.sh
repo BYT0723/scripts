cac7f7a4a3d8a5207160c63e26f94f82c2eef5cf
BYT0723 <twang9739@gmail.com>
2024-04-17 10:40:12 +0800

    feat: autostart set xorg keyboard

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,12 @@
 
 dir=$(dirname $0)
 
+# Set Xorg Keymap
+if [ ! -z "$(pgrep Xorg)" ] && [ ! -z "$(command -v setxkbmap)" ]; then
+	setxkbmap -option "caps:swapescape,altwin:swap_lalt_lwin" # setxkbmap need `xorg-xkb-utils` package
+	# For other keymaps, see: `/usr/share/X11/xkb/rules/base.lst`
+fi
+
 # statusBar
 if [ -z "$(pgrep -f dwm-status.sh)" ]; then
 	/bin/bash $dir/dwm-status.sh &
@@ -49,4 +55,17 @@ fi
 # personal note
 if [ -z "$(pgrep -f note.sh)" ]; then
 	/bin/bash $dir/note.sh &
+fi
+
+# Set Xorg Keyboard Configuration
+if [ ! -z "$(pgrep Xorg)" ] && [ ! -z "$(command -v xset)" ]; then
+	expected_delay=200
+	expected_rate=30
+	msg=$(xset q | grep 'auto repeat delay') # xset need `xorg-xset` package
+	cur_delay=$(echo $msg | awk '{print $4}')
+	cur_rate=$(echo $msg | awk '{print $NF}')
+
+	if [ "$cur_delay" -ne "$expected_delay" ] || [ "$cur_rate" -ne "$expected_rate" ]; then
+		xset r rate $expected_delay $expected_rate
+	fi
 fi
