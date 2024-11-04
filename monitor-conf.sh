414783b64bd534a868deff6651e6f11d20974752
BYT0723 <twang9739@gmail.com>
2024-11-05 00:47:15 +0800

    perf: monitor conf for sync refresh rate

--- monitor-conf.sh
+++ monitor-conf.sh
@@ -5,4 +5,4 @@
 
 xrandr \
 	--output eDP --primary --mode 3200x2000 --rate 120 --rotate normal --scale 0.8x0.8 \
-	--output HDMI-A-0 --mode 2560x1440 --rate 144 --left-of eDP
+	--output HDMI-A-0 --mode 2560x1440 --rate 120 --left-of eDP
