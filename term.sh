88b40324843770269438dae36ab4e89015e201e8
BYT0723 <twang9739@gmail.com>
2024-11-29 00:01:09 +0800

    fix: st defaultattr overwrite foreground color

--- term.sh
+++ term.sh
@@ -8,7 +8,7 @@ Type=$1
 # new different termina by $Type
 case "$Type" in
 float)
-	st -i -g 130x40 -f "CaskaydiaCove Nerd Font:style=Regular:size=10" &
+	st -i -g 130x40 -f "CaskaydiaCove Nerd Font:size=10" &
 	;;
 *)
 	WINIT_X11_SCALE_FACTOR=1 alacritty -o 'font.size=17' &
