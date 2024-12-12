c2045661699cb97f37b57868886f11d571cc720e
BYT0723 <twang9739@gmail.com>
2024-12-12 18:08:06 +0800

    feat: rofi-emoji script
    perf

---
+++ emoji.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya (adi1090x)
+## Github  : @adi1090x
+#
+## Applets : Quick Links
+
+if [ -z "$(command -v rofi)" ]; then
+	notify-send "please install rofi"
+	exit 0
+fi
+
+if [ -z "$(rofi -h | grep emoji)" ]; then
+	notify-send "please install rofi-emoji"
+	exit 0
+fi
+
+dir=$(dirname $0)
+
+# Import Current Theme
+type="$dir/rofi/launchers/type-3"
+style='style-4.rasi'
+theme="$type/$style"
+efonts="Twemoji 40"
+
+# Theme Elements
+prompt='Emoji'
+mesg="Search a emoji"
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -modi emoji -show emoji \
+		-theme-str "element-text {font: \"$efonts\";}" \
+		-theme-str "element-icon {size: 0px;}" \
+		-theme-str "configuration { display-emoji: \" \";}" \
+		-emoji-mode $1 \
+		-emoji-format '{emoji}' \
+		-p "$prompt" \
+		-mesg "$mesg" \
+		-markup-rows \
+		-theme ${theme}
+}
+
+case "$1" in
+"-o")
+	rofi_cmd stdout
+	;;
+*)
+	rofi_cmd insert
+	;;
+esac
