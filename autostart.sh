0a05777508389e43e95189c1c648feef025a914f
BYT0723 <twang9739@gmail.com>
2025-02-19 01:37:53 +0800

    perf: autostart script

--- autostart.sh
+++ autostart.sh
@@ -2,63 +2,65 @@
 
 dir=$(dirname $0)
 
-# Set Xorg
-if [ ! -z "$(pgrep Xorg)" ]; then
-	# Set Xorg Keyboard Configuration
-	if [ ! -z "$(command -v setxkbmap)" ]; then
-		# For other keymaps, see: `/usr/share/X11/xkb/rules/base.lst`
-		setxkbmap -option "caps:swapescape,altwin:swap_lalt_lwin" # setxkbmap need `xorg-xkb-utils` package
+# 启动应用
+# $1 application name
+# $2 command
+launch() {
+	[ -z "$(pgrep $1)" ] && eval "$2" || true
+}
+
+# 启动并监控脚本
+# 防止在修改脚本时出错导致进程退出
+# $1 script name
+# $2 command
+launch_monitor() {
+	while true; do
+		[ -z "$(pgrep -f $1)" ] && eval "$2" || true
+		# 每分钟
+		sleep 60
+	done
+}
+
+xorg_setting() {
+	# Set Xorg
+	if [ ! -z "$(pgrep Xorg)" ]; then
+		# Set Xorg Keyboard Configuration
+		if [ ! -z "$(command -v setxkbmap)" ]; then
+			# For other keymaps, see: `/usr/share/X11/xkb/rules/base.lst`
+			setxkbmap -option "caps:swapescape,altwin:swap_lalt_lwin" # setxkbmap need `xorg-xkb-utils` package
+		fi
+		if [ ! -z "$(command -v xset)" ]; then
+			xset r rate 250 35
+		fi
 	fi
-	if [ ! -z "$(command -v xset)" ]; then
-		xset r rate 250 35
-	fi
+	# 壁纸(不用判断是否存在，脚本中已判断)
+	/bin/bash $dir/wallpaper.sh -r &
 
-	# screen manager (屏保和DPMS)
-	if [ -z "$(pgrep -f screen.sh)" ]; then
-		/bin/bash $dir/screen.sh &
-	fi
-fi
+	# 状态栏信息
+	launch_monitor "[d]wm-status.sh" "/bin/bash $dir/dwm-status.sh &" &
+	# 壁纸
+	launch_monitor "[w]allpaper.sh" "/bin/bash $dir/wallpaper.sh -r &" &
+	# 屏保
+	launch_monitor "[s]creen.sh" "/bin/bash $dir/screen.sh &" &
+}
 
-# 启动通知
-if [ -z "$(pgrep dunst)" ]; then
-	dunst &
-fi
+application_launch() {
+	# 窗口合成器 picom (window composer)
+	# picom --config $dir/configs/picom.conf -b --experimental-backends (开启试验功能)
+	launch picom "picom --config $dir/configs/picom.conf -b"
+	# 启动通知
+	launch dunst "dunst &"
+	# network manager 网络管理bar icon
+	launch nm-applet "nm-applet &"
+	# input method
+	launch fcitx5 "fcitx5 -d"
+	# 音频控制
+	launch easyeffects "easyeffects --gapplication-service &"
+	# auto mount
+	launch udiskie "udiskie -sn &"
+	# polkit (require lxsession or lxsession-gtk3) 鉴权
+	launch lxpolkit "lxpolkit &"
+}
 
-# 音频控制
-if [ -z "$(pgrep easyeffects)" ]; then
-	easyeffects --gapplication-service &
-fi
-
-# 状态栏
-if [ -z "$(pgrep -f dwm-status.sh)" ]; then
-	/bin/bash $dir/dwm-status.sh &
-fi
-
-# 壁纸
-/bin/bash $dir/wallpaper.sh -r &
-
-# picom (window composer) 窗口合成
-if [ -z "$(pgrep picom)" ]; then
-	picom --config $dir/configs/picom.conf -b
-	# picom --config $dir/configs/picom.conf -b --experimental-backends
-fi
-
-# polkit (require lxsession or lxsession-gtk3) 鉴权
-if [ -z "$(pgrep lxpolkit)" ]; then
-	lxpolkit &
-fi
-
-# network manager 网络管理bar icon
-if [ -z "$(pgrep nm-applet)" ]; then
-	nm-applet &
-fi
-
-# auto mount
-if [ -z "$(pgrep udiskie)" ]; then
-	udiskie -sn &
-fi
-
-# input method
-if [ -z "$(pgrep fcitx5)" ]; then
-	fcitx5 -d
-fi
+xorg_setting
+application_launch
