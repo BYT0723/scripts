5f92a258477654c310c20cae64d725e3dfb6d486
BYT0723 <twang9739@gmail.com>
2025-12-22 12:01:23 +0800

    fix: random_file

--- tools/random_file.sh
+++ tools/random_file.sh
@@ -8,6 +8,8 @@ len=${2:-10}
 [ -z "$dir" ] && echo "Usage: $(basename $0) <dir> [len]" && exit 1
 [ ! -d "$dir" ] && echo "$dir is not a directory" && exit 1
 
+[[ "$dir" != /* ]] && dir=$(realpath "$dir")
+
 (
 	cd "$dir"
 	mpv \
