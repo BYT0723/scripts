7cb0f1a68f579cb308d7ed739fe871bdfcecd308
BYT0723 <twang9739@gmail.com>
2026-01-03 18:05:09 +0800

    perf: brightness script

--- tools/brightness.sh
+++ tools/brightness.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 #
 # Required
-# light
-#
-# Remember to add yourself to the video user group
-# https://wiki.archlinux.org/title/Backlight#light
+# brightnessctl
 #
 msgTag="brightness"
 
 source "$(dirname $0)/../utils/notify.sh"
 
-[ -z "$(command -v light)" ] && system-notify critical "Tool Not Found" "please install light-git from archlinuxcn" && exit
-
-curr=$(printf '%.0f' $(light -G))
+[ -z "$(command -v brightnessctl)" ] && system-notify critical "Tool Not Found" "please install brightnessctl" && exit
 
 case "$1" in
 'up')
-	light -S $(echo "$curr+2" | bc)
+	brightnessctl s +2%
 	;;
 'down')
-	light -S $(echo "$curr-2" | bc)
+	brightnessctl s 2%-
 	;;
 esac
 
-# notify-send -c tools -i display-brightness-symbolic -h string:x-dunst-stack-tag:$msgTag "$(printf "%.0f" $(light -G))"
-# # support progress bar
-notify-send -c tools -i display-brightness-symbolic -h string:x-dunst-stack-tag:$msgTag -h int:value:"$(printf "%.0f" $(light -G))" "$(printf "%.0f" $(light -G))"
+curr=$(printf "%.0f" $(echo "$(brightnessctl g)*100/$(brightnessctl m)" | bc))
+
+notify-send -c tools -i display-brightness-symbolic -h string:x-dunst-stack-tag:$msgTag -h int:value:"$curr" "$curr"
