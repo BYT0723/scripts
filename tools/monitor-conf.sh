6b7070ec26c1ad53f351f41d4900c8e93c6a34b6
BYT0723 <twang9739@gmail.com>
2026-03-15 05:05:13 +0800

    perf: monitor-conf

--- tools/monitor-conf.sh
+++ tools/monitor-conf.sh
@@ -4,10 +4,10 @@
 # Lightdm: session-setup-script=/home/<username>/.dwm/tools/monitor-conf.sh
 
 # 屏幕布局
-# cmd="xrandr --output eDP --primary --mode 3200x2000 --rate 120.00 --rotate normal --scale 0.8x0.8"
-cmd="xrandr --output eDP --primary --mode 3200x2000 --rate 144.00 --rotate normal --scale 0.8x0.8"
+cmd="xrandr --output eDP --primary --mode 3200x2000 --rate 120.00 --rotate normal"
 
 if xrandr | grep -q "HDMI-A-0 connected"; then
+	cmd="xrandr --output eDP --primary --mode 1920x1200 --rate 144.00 --rotate normal --scale 1.2x1.2"
 	cmd+=" --output HDMI-A-0 --mode 2560x1440 --rate 144.00 --rotate normal --left-of eDP"
 fi
 
