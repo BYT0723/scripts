60d7efda9f69bf0bca742f76ec5d8bbdf11b7cb7
BYT0723 <twang9739@gmail.com>
2026-03-30 10:00:54 +0800

    perf: clock script
    fix: environment

--- tools/clock.sh
+++ tools/clock.sh
@@ -3,43 +3,40 @@
 #
 # Clock.sh
 #
-# format: <minute> <hour> <day> <month> <week> /home/<name>/clock.sh [WORK|HOME|DIET|STUDY] <Message>
+# format: <minute> <hour> <day> <month> <week> /home/<name>/clock.sh -l <level> -c [WORK|HOME|DIET|STUDY] <Message>
 # Use `crontab -e` to add alarm clock items
 
+LEVEL="critical"
+CATEGORY=""
+TAG="x-custom-clock-$(echo "$CATEGORY|$MESG" | md5sum | cut -d' ' -f1)"
+
+while getopts "l:c:" opt; do
+	case $opt in
+	l) LEVEL=$OPTARG ;;
+	c) CATEGORY=$OPTARG ;;
+	?) exit 1 ;;
+	esac
+done
+
+shift $((OPTIND - 1))
+
+MESG=${1:-""}
+
+declare -A icons
+
+icons["WORK"]="  "
+icons["HOME"]="  "
+icons["STUDY"]="  "
+icons["DIET"]="󰩰  "
+icons["PLAN"]="  "
+
+[ -n "${icons["$CATEGORY"]}" ] && ICON="${icons["$CATEGORY"]}"
+
+(canberra-gtk-play -i audio-volume-change >/dev/null 2>&1 &)
+
+### Send Notification
 # Because this script is called by the cronie service, it needs to export DBUS_SESSION_BUS_ADDRESS
 # If DBUS_SESSION_BUS_ADDRESS is invalid, use `echo $DBUS_SESSION_BUS_ADDRESS` to get new DBUS_SESSION_BUS_ADDRESS
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-# Displayed screen
-export DISPLAY=:0
-
-LEVEL="normal"
-ICON=""
-TAG="clock"
-
-case "$1" in
-"WORK")
-	ICON=" "
-	;;
-"HOME")
-	ICON=" "
-	;;
-"STUDY")
-	ICON=" "
-	;;
-"DIET")
-	ICON="󰩰 "
-	;;
-"PLAN")
-	LEVEL="low"
-	ICON=" "
-	TAG="note"
-	;;
-*)
-	echo "$1"
-	;;
-esac
-
-canberra-gtk-play -i audio-volume-change
-
-# send notification
-notify-send -i preferences-system-time-symbolic -h string:x-dunst-stack-tag:$msgTag -u "$LEVEL" "$ICON  $(date +"%H:%M")" "$2"
+DBUS_SESSION_BUS_ADDRESS=${DBUS_SESSION_BUS_ADDRESS:-"unix:path=/run/user/1000/bus"} \
+	DISPLAY=${DISPLAY:-:0} \
+	timeout 1 notify-send -i preferences-system-time-symbolic -h string:x-dunst-stack-tag:$TAG -u "$LEVEL" "$ICON$(date +"%H:%M")" "$MESG"
