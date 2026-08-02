ac62324710d951d35d56d0b74b3d5391a01e8342
BYT0723 <twang9739@gmail.com>
2026-08-02 15:03:11 +0800

    refactor(screenshot): replace maim with flameshot gui, add clipboard preview

--- rofi/scripts/screenshot.sh
+++ rofi/scripts/screenshot.sh
@@ -1,71 +1,58 @@
 #!/usr/bin/env bash
 
 ROFI_DIR="$(dirname "$(dirname "$0")")"
-
 MODULE_THEME="$ROFI_DIR/applets/type-1/style-2.rasi"
-MODULE_NAME=" Screenshot"
+MODULE_NAME="Screenshot"
 MODULE_MESG="screen capture"
+VIEWER="nsxiv"
 
 source "$(dirname "$0")"/util.sh
 source "$(dirname "$0")"/lib-module.sh
-
-DIR="$(xdg-user-dir)/Screenshots"
-[[ ! -d "$DIR" ]] && mkdir -p "$DIR"
-
-time=$(date +%Y-%m-%d-%H-%M-%S)
-geometry=$(xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current')
-FILENAME="Screenshot_${time}_${geometry}.png"
-appName="Screenshot"
+source "$(dirname "$ROFI_DIR")/utils/monitor.sh"
 
 module_parse <<MODULES
-flameshot||Flameshot||
 desktop||Capture Desktop||
 area||Capture Area||
 window||Capture Window||
 timer||Capture in 5s||
 MODULES
 
-notify_view() {
-	notify-send -i gscreenshot -c history-ignore -u low --replace-id=699 $appName "Copied to clipboard."
-	feh $DIR/"$FILENAME"
-	if [[ -e "$DIR/$FILENAME" ]]; then
-		notify-send -i gscreenshot -c history-ignore -u low --replace-id=699 $appName "Screenshot Saved."
-	else
-		notify-send -i gscreenshot -c history-ignore -u low --replace-id=699 $appName "Screenshot Deleted."
+shot_and_preview() {
+	tmpfile="/tmp/screenshot_preview.png"
+	before_md5=$(xclip -selection clipboard -t image/png -o 2>/dev/null | md5sum | cut -d' ' -f1)
+
+	"$@"
+
+	if ! xclip -selection clipboard -t image/png -o >"$tmpfile" 2>/dev/null; then
+		return 0
 	fi
+	after_md5=$(md5sum "$tmpfile" | cut -d' ' -f1)
+	if [ "$before_md5" != "$after_md5" ]; then
+		$VIEWER "$tmpfile"
+	fi
+	rm -f "$tmpfile"
 }
 
-copy_shot() { tee "$FILENAME" | xclip -selection clipboard -t image/png; }
+handle_desktop() {
+	read -r _ _ w h _ _ < <(get_current_monitor)
+	shot_and_preview flameshot gui --region "${w}x${h}+0+0"
+}
+handle_area() { shot_and_preview flameshot gui; }
+handle_window() {
+	tmpfile="/tmp/screenshot_preview.png"
+	before_md5=$(xclip -selection clipboard -t image/png -o 2>/dev/null | md5sum | cut -d' ' -f1)
 
-countdown() {
-	for sec in $(seq $1 -1 1); do
-		notify-send -i gscreenshot -c history-ignore -t 1010 --replace-id=699 $appName "Taking shot in : $sec"
-		sleep 1
-	done
-}
+	maim -u -i "$(xdotool getactivewindow)" | xclip -selection clipboard -t image/png
 
-shotnow() {
-	cd $DIR && sleep 0.5 && maim -u -f png | copy_shot
-	notify_view
+	if ! xclip -selection clipboard -t image/png -o >"$tmpfile" 2>/dev/null; then
+		return 0
+	fi
+	after_md5=$(md5sum "$tmpfile" | cut -d' ' -f1)
+	if [ "$before_md5" != "$after_md5" ]; then
+		$VIEWER "$tmpfile"
+	fi
+	rm -f "$tmpfile"
 }
-shot5() {
-	countdown '5'
-	sleep 1 && cd $DIR && maim -u -f png | copy_shot
-	notify_view
-}
-shotwin() {
-	cd $DIR && maim -u -f png -i $(xdotool getactivewindow) | copy_shot
-	notify_view
-}
-shotarea() {
-	cd $DIR && maim -u -f png -s -b 2 -c 0.35,0.55,0.85,0.25 -l | copy_shot
-	notify_view
-}
-
-handle_flameshot() { flameshot gui; }
-handle_desktop() { shotnow; }
-handle_area() { shotarea; }
-handle_window() { shotwin; }
-handle_timer() { shot5; }
+handle_timer() { sleep 5 && handle_desktop; }
 
 module_loop
