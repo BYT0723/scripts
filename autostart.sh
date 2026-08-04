79638266f38664420654c30542fa07a756b66d3e
BYT0723 <twang9739@gmail.com>
2026-08-04 18:06:28 +0800

    feat: enable autorandr change in autostart

--- autostart.sh
+++ autostart.sh
@@ -7,7 +7,7 @@ TOOLS_DIR="$WORK_DIR/tools"
 CONKY_AUTOSTART=1
 
 # 显示器布局初始化
-# [ -n "$(command -v autorandr)" ] && autorandr --change
+[ -n "$(command -v autorandr)" ] && autorandr --change
 
 # 启动应用
 # $1 policy           string [check/restart]
